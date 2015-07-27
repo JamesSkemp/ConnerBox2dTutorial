ae9661508dacd630f6902e30a976a5f1d89922bf
James Skemp <strivinglife@gmail.com>
2015-07-27 18:17:44 -0500

    Created new libGDX 1.6.4 project.

---
+++ android/src/com/jamesrskemp/connerbox2d/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jamesrskemp.connerbox2d.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jamesrskemp.connerbox2d.ConnerBox2d;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new ConnerBox2d(), config);
+	}
+}
---
+++ desktop/src/com/jamesrskemp/connerbox2d/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jamesrskemp.connerbox2d.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jamesrskemp.connerbox2d.ConnerBox2d;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new ConnerBox2d(), config);
+	}
+}
