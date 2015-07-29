203c00e0a6768b1b15ba85f08a027b19f1302da5
James Skemp <strivinglife@gmail.com>
2015-07-28 19:42:36 -0500

    Added part of first video (with my own tweaks) to add a falling box.

--- core/src/com/jamesrskemp/connerbox2d/ConnerBox2d.java
+++ core/src/com/jamesrskemp/connerbox2d/ConnerBox2d.java
@@ -3,11 +3,25 @@ package com.jamesrskemp.connerbox2d;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
+import com.badlogic.gdx.physics.box2d.PolygonShape;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class ConnerBox2d extends ApplicationAdapter {
 	private static final String TAG = ConnerBox2d.class.toString();
+
+	OrthographicCamera camera;
+
+	World world;
+	Box2DDebugRenderer box2dRenderer;
+
+	Body player;
 
 	SpriteBatch batch;
 	Texture img;
@@ -17,28 +31,70 @@ public class ConnerBox2d extends ApplicationAdapter {
 		float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
 
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		// Wants a zoomed in view.
+		camera.setToOrtho(false, w / 2, h / 2);
+
+		world = new World(new Vector2(0, -9.8f), false);
+		box2dRenderer = new Box2DDebugRenderer();
+
+		player = createPlayer();
+
+//		batch = new SpriteBatch();
+//		img = new Texture("badlogic.jpg");
 
 		Gdx.app.log(TAG, "asdf");
 	}
 
 	@Override
 	public void render () {
+		update(Gdx.graphics.getDeltaTime());
+
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+
+		box2dRenderer.render(world, camera.combined);
+//		batch.begin();
+//		batch.draw(img, 0, 0);
+//		batch.end();
 	}
 
 	@Override
 	public void resize(int width, int height) {
 		super.resize(width, height);
+		camera.setToOrtho(false, width / 2, height / 2);
 	}
 
 	@Override
 	public void dispose() {
 		super.dispose();
+		world.dispose();
+		box2dRenderer.dispose();
+	}
+
+	public void update(float deltaTime) {
+		// 6 and 2 seem to be the standard for these.
+		world.step(1 / 60f, 6, 2);
+	}
+
+	public Body createPlayer() {
+		Body body;
+
+		BodyDef bodyDef = new BodyDef();
+		bodyDef.type = BodyDef.BodyType.DynamicBody;
+		bodyDef.position.set(100, 100);
+		bodyDef.fixedRotation = true;
+
+		body = world.createBody(bodyDef);
+
+		PolygonShape shape = new PolygonShape();
+		// Box2D will double what we pass for the size.
+		shape.setAsBox(32 / 2, 32 / 2);
+
+		body.createFixture(shape, 1.0f);
+
+		shape.dispose();
+
+		return body;
 	}
 }
