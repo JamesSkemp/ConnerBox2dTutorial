7f688d46991a4f5ce94338db747a7572727eec76
James Skemp <strivinglife@gmail.com>
2015-07-28 19:20:34 -0500

    Initial desktop config updates.

--- core/src/com/jamesrskemp/connerbox2d/ConnerBox2d.java
+++ core/src/com/jamesrskemp/connerbox2d/ConnerBox2d.java
@@ -7,21 +7,38 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class ConnerBox2d extends ApplicationAdapter {
+	private static final String TAG = ConnerBox2d.class.toString();
+
 	SpriteBatch batch;
 	Texture img;
 	
 	@Override
 	public void create () {
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
+
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+
+		Gdx.app.log(TAG, "asdf");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, 0, 0);
 		batch.end();
+	}
+
+	@Override
+	public void resize(int width, int height) {
+		super.resize(width, height);
+	}
+
+	@Override
+	public void dispose() {
+		super.dispose();
 	}
 }
--- desktop/src/com/jamesrskemp/connerbox2d/desktop/DesktopLauncher.java
+++ desktop/src/com/jamesrskemp/connerbox2d/desktop/DesktopLauncher.java
@@ -7,6 +7,14 @@ import com.jamesrskemp.connerbox2d.ConnerBox2d;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.width = 720;
+		config.height = 480;
+		config.title = "Conner Box2D Tutorial";
+
+		// TODO remove or change later
+		config.backgroundFPS = 60;
+		config.foregroundFPS = 60;
+
 		new LwjglApplication(new ConnerBox2d(), config);
 	}
 }
